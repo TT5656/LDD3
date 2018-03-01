34f348c496567dfa9a8763c55c2e92404bfd0bbd
Steven Wang <steven.wang@arm.com>
2018-03-01 12:02:38 +0800

    add scripts to ease environment setup and module building
    Add setup.sh to load cross compiler from /arm/tools.
    Add make.sh to automate make for hikey960_debian kernel.

---
+++ make.sh
@@ -0,0 +1,6 @@
+. ./setup.sh
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+make
---
+++ setup.sh
@@ -0,0 +1,3 @@
+. /arm/tools/setup/init/bash
+module load swdev
+module load arm/gcc_aarch64/4.9_2014.09
